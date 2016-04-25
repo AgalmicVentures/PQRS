45544ef06b54f384626b71c5dfb5f37228b7b13c
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-04-24 19:37:42 -0500

    Added safety flags to shell scripts

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -u
+
 CWD=`pwd`
 PROCESSES=`ps xa | grep $CWD/PQRS/__init__.py | grep -v grep`
 if [[ $? -eq 0 ]]; then
 	echo "Already running"
 	exit
 fi
+
+set -e
 
 echo "Starting..."
 nohup $CWD/PQRS/__init__.py &
--- stop.sh
+++ stop.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -u
+
 CWD=`pwd`
 PROCESSES=`ps xa | grep $CWD/PQRS/__init__.py | grep -v grep`
 if [[ $? -eq 1 ]]; then
 	echo "Not running"
 	exit
 fi
+
+set -e
 
 PID=`echo $PROCESSES | awk '{print $1}'`
 kill $@ $PID
